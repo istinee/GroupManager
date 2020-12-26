a2c4d96c8c091572c6dab07239d9c5902382744a
istinee <76502944+istinee@users.noreply.github.com>
2020-12-27 05:17:35 +0700

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -29,7 +29,7 @@ I'm group manager bot and Anti-spam for RR Players🃏
 Activate Anti-spam protection in your BM by this command : 
 /antispam on 
 
-Made from the source code of [isti nee](t.me/jabami_yumekoo_chan)
+bot by [isti nee](t.me/jabami_yumekoo_chan)
 
 Special thanks to [this person](t.me/denver02) for helping me.
 
